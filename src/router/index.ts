5a4c16ca9d74b520e22f36d467b0117d96728ebd
German <german.suvorov134@gmail.com>
2024-02-02 18:09:19 +0300

    feat: add getCompanyService, CompanyDetail component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import Home from "@/views/Home.vue";
+import CompanyDetails from "@/views/CompanyDetails.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +8,12 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: HomeView,
+      component: Home,
+    },
+    {
+      path: "/companies/:id",
+      name: "CompanyDetails",
+      component: CompanyDetails,
     },
   ],
 });
--- src/services/api/company/companyAPI.ts
+++ src/services/api/company/companyAPI.ts
@@ -1,9 +1,22 @@
 import axios from "axios";
 import type { ICompany } from "@/stores/types/types";
 
-export const createCompany = async (name: string) => {
+export const createCompanyService = async (name: string) => {
   const res = await axios
     .post<ICompany>(`${import.meta.env.VITE_API_URL}/companies`, { name })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      throw new Error(error);
+    });
+
+  return res;
+};
+
+export const getCompanyService = async (id: number) => {
+  const res = await axios
+    .get<ICompany>(`${import.meta.env.VITE_API_URL}/companies/${id}`)
     .then((response) => {
       return response.data;
     })
